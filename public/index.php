977ea21abc649c77d1b7d58f5be314266123f424
Dima <bugaydima@gmail.com>
2022-04-01 00:22:39 +0300

    router

--- public/index.php
+++ public/index.php
@@ -3,8 +3,7 @@ if (PHP_MAJOR_VERSION < 8){
     die("Need PHP version >= 8");
 }
 require_once dirname(__DIR__) . '/config/init.php';
+require_once CONFIG . '/routes.php';
 
 new \prj\App();
 
-throw new Exception('Error', 404);
-//echo $test;
---
+++ vendor/prj/Router.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace prj;
+
+class Router 
+{
+    protected static array $routes = [];
+    protected static array $route = [];
+
+    public static function add($regexp, $route = [])
+    {
+        self::$routes[$regexp] = $route;
+    }
+
+}
